f9be4fd1957c80cf5180594de443a345a6c5a98f
Sophia Kruk Andraski <sophia_andraski@estudante.sesisenai.org.br>
2026-08-30 19:23:07 -0300

    30/08

--- Yngrid e Sophia/script.js
+++ Yngrid e Sophia/script.js
@@ -1,225 +1,177 @@
-function termo (){
-    const acerteSenha =document.querySelector (".acerteSenha")
+function termo() {
+
+    const acerteSenha = document.querySelector(".acerteSenha")
 
     for (let c = 1; c <= 6; c++) {
-        for ( let i = 1;i <= 5;i++){
-            let quadrado = document.createElement ("div")
-            quadrado.classList.add ("quadrado")
+
+        for (let i = 1; i <= 5; i++) {
+
+            let quadrado = document.createElement("div")
+
+            quadrado.classList.add("quadrado")
 
             acerteSenha.appendChild(quadrado)
-
         }
     }
 }
 
-termo ()
+termo()
 
 const arrayPalavras = [
     "jogos",
     "porta",
     "amora",
     "carro",
-    "mundo"
-    
+    "mundo",
+    "selva",
+    "pulga",
+    "cinto",
+    "ferro"
 ]
-let palavraSorteada;
 
-const frase = document.createElement ("P")
+let palavraSorteada
 
+function sortearFrase() {
 
-function sortearFrase () {
-    let indice = Math.floor(Math.random() * arrayPalavras.length)
+    let indice =
+        Math.floor(
+            Math.random() *
+            arrayPalavras.length
+        )
 
-    return palavraSorteada = arrayPalavras[indice]
+    palavraSorteada = arrayPalavras[indice]
 
 }
 
 sortearFrase()
+
 console.log(palavraSorteada)
 
+let btnEnviar = document.querySelector(".btnEnviar")
 
-let btnEnviar = document.querySelector (".btnEnviar")
-let inputText = document.querySelector (".inputText")
+let inputText = document.querySelector(".inputText")
+
+let numero = document.querySelector(".num")
+
+const btnReiniciar = document.querySelector(".btnReiniciar")
+
 inputText.maxLength = 5
+
 let posicaoLetra = 0
 
 let atual = 0
-let numero = document.querySelector (".num")
-const btnReiniciar = document.querySelector(".btnReiniciar") 
 
-btnEnviar.addEventListener ("click", function(){
-    verificar()
-})
-
-inputText.addEventListener ("keydown", function (event) {
-    if (event.key === "Enter")
-        verificar()
-})
-
-function verificar (){
-    
-    let input = document.querySelector (".inputText")
-    let texto = input.value.trim()
-    
-    if (texto.length < 5) {
-        return
-    }
-
-    let quadrado = document.querySelectorAll(".quadrado")
-
-    for (let i = 0; i < 5; i++){
-        let num = quadrado[posicaoLetra]
-        num.classList.remove("certo", "erro", "meioCerto")
-        num.textContent = texto[i]
-
-        if (texto[i] == palavraSorteada[i]){
-            num.classList.add("certo")
-        } 
-
-        else if (
-            texto[i] == palavraSorteada[0] || 
-            texto[i] == palavraSorteada[1] || 
-            texto[i] == palavraSorteada[2] || 
-            texto[i] == palavraSorteada[3] || 
-            texto[i] == palavraSorteada[4]
-        ) {
-            num.classList.add("meioCerto")
-        }
-
-        else {
-            num.classList.add("erro")
-        }
-        posicaoLetra++
-        
-
-    }
-
-    atual++
-    numero.textContent = atual + ""
-
-
-    input.value = ""
-}
 
 const SYmodalVitoria = document.querySelector("#SYmodalVitoria")
+
 const SYmodalDerrota = document.querySelector("#SYmodalDerrota")
-const SYbtnFechar = document.querySelector("#SYmodalVitoria.SYbtnFechar")
-const SYbtnFechar2 = document.querySelector("#SYmodalDerrota .SYbtnReiniciar2")
+
+
+const SYbtnFechar = document.querySelector("#SYmodalVitoria .SYbtnFechar")
+
+const SYbtnFechar2 = document.querySelector("#SYmodalDerrota .SYbtnFechar2")
+
 
 btnEnviar.addEventListener("click", function() {
-    verificar()
-})
-
-inputText.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
         verificar()
     }
+)
 
-})
+inputText.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            verificar()
+        }
+    }
+)
+
 function verificar() {
-    let texto = inputText.value.trim().toLowerCase()
+    let input =
+        document.querySelector(".inputText")
+
+    let texto =
+        input.value.trim().toLowerCase()
+
     if (texto.length < 5) {
         return
     }
 
+
     let quadrado = document.querySelectorAll(".quadrado")
+
     for (let i = 0; i < 5; i++) {
-        let num = quadrado[posicaoLetra]
+        let num =
+            quadrado[posicaoLetra]
+
         num.classList.remove(
             "certo",
             "erro",
             "meioCerto"
         )
 
-
         num.textContent = texto[i]
 
         if (texto[i] == palavraSorteada[i]) {
-
             num.classList.add("certo")
-
-        } else if (
-
+        }else if (
             texto[i] == palavraSorteada[0] ||
-
             texto[i] == palavraSorteada[1] ||
-
             texto[i] == palavraSorteada[2] ||
-
             texto[i] == palavraSorteada[3] ||
-
-            texto[i] == palavraSorteada[4]
-
-        ) {
-
+            texto[i] == palavraSorteada[4]) {
             num.classList.add("meioCerto")
-
-        } else {
-
+        }else {
             num.classList.add("erro")
-
         }
-
-
         posicaoLetra++
-
     }
 
     atual++
 
-    numero.textContent = atual + ""
+    numero.textContent =
+        atual + ""
 
-    inputText.value = ""
+
+    input.value = ""
 
     if (texto === palavraSorteada) {
 
         SYmodalVitoria.classList.add("is-active")
-
         return
 
-    } else if (atual === 6) {
-
+    } 
+    if (atual === 6) {
         SYmodalDerrota.classList.add("is-active")
-
         return
-
     }
-
 }
 
 SYbtnFechar.addEventListener("click", function() {
-
-    SYmodalVitoria.classList.remove("is-active")
-
-})
-
+        SYmodalVitoria.classList.remove("is-active")
+    }
+)
 SYbtnFechar2.addEventListener("click", function() {
-
-    SYmodalDerrota.classList.remove("is-active")
-
-})
+        SYmodalDerrota.classList.remove("is-active")
+    }
+)
 
 btnReiniciar.addEventListener("click", function() {
-
-    reiniciarJogo()
-
-})
+        reiniciarJogo()
+    }
+)
 
 function reiniciarJogo() {
-
-    posicaoLetra = 0
 
     atual = 0
 
     numero.textContent = "0"
 
+    posicaoLetra = 0
+
     inputText.value = ""
 
     sortearFrase()
 
-    let quadrado =
-        document.querySelectorAll(".quadrado")
-
+    let quadrado = document.querySelectorAll(".quadrado")
 
     for (let i = 0; i < 30; i++) {
 
@@ -230,54 +182,14 @@ function reiniciarJogo() {
             "erro",
             "meioCerto"
         )
-
     }
 
-    SYmodalVitoria.classList.remove("is-active")
+    SYmodalVitoria.classList.remove(
+        "is-active"
+    )
 
-    SYmodalDerrota.classList.remove("is-active")
+    SYmodalDerrota.classList.remove(
+        "is-active"
+    )
 
 }
-
-SYbtnFechar.addEventListener("click", function () {
-    SYmodalVitoria.classList.remove("is-active")
-})
-
-SYbtnFechar2.addEventListener("click", function () {
-    SYmodalDerrota.classList.remove("is-active")
-})
-
-btnReiniciar.addEventListener("click", reiniciarJogo)
-
-function reiniciarJogo() {
-    posicaoLetra = 0
-    atual = 0
-    numero.textContent = "0"
-    inputText.value = ""
-
-    sortearFrase()
-
-    let quadrado = document.querySelectorAll(".quadrado");
-    for (let i = 0; i < 30; i++) {
-        quadrado[i].textContent = "";
-        quadrado[i].classList.remove("certo", "erro", "meioCerto");
-    }
-
-    SYmodalVitoria.classList.remove("is-active")
-    SYmodalDerrota.classList.remove("is-active")
-}
-
-SYbtnFechar.addEventListener("click", function() {
-    reiniciarJogo()
-})
-
-SYbtnFechar2.addEventListener("click", function() {
-    reiniciarJogo()
-})
-
-
-//Duplicações em massa: A função verificar(), a função reiniciarJogo() e a maioria dos eventos (addEventListener) estão declarados 2 a 3 vezes no mesmo arquivo. Isso gera conflitos e faz cada tentativa contar em dobro.
-
-//Erro de Seletor (Uncaught TypeError): A linha querySelector("#SYmodalVitoria.SYbtnFechar") está sem o espaço entre o ID e a classe. O navegador tenta achar um elemento com o ID e a classe juntos, não encontra (retorna null), e travará o JavaScript na hora de tentar adicionar o ouvinte de clique.
-
-//Looping Infinito do Quadrado: Ao reiniciar, se a função termo() não for limpa antes, a tela acumulará blocos infinitamente a cada partida.
