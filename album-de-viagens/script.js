656d52da69c9f539d409e1cf2e1ea01f55cc7c52
SOPHIA KRUK ANDRASKI <sophia_andraski@estudante.sesisenai.org.br>
2026-09-02 16:33:12 -0300

    02/08

---
+++ album-de-viagens/script.js
@@ -0,0 +1,44 @@
+const quadro01 = document.querySelector(".quadro-01");
+quadro01.addEventListener("click", function(){
+    quadro01.classList.toggle("verde")
+})
+
+const quadro02 = document.querySelector(".quadro-02");
+quadro02.addEventListener("click", function(){
+    quadro02.classList.toggle("verde")
+})
+
+const quadro03 = document.querySelector(".quadro-03");
+quadro03.addEventListener("click", function(){
+    quadro03.classList.toggle("verde")
+})
+
+const quadro04 = document.querySelector(".quadro-04");
+quadro04.addEventListener("click", function(){
+    quadro04.classList.toggle("verde")
+})
+
+const quadro05 = document.querySelector(".quadro-05");
+quadro05.addEventListener("click", function(){
+    quadro05.classList.toggle("verde")
+})
+
+const quadro06 = document.querySelector(".quadro-06");
+quadro06.addEventListener("click", function(){
+    quadro06.classList.toggle("verde")
+})
+
+const quadro07 = document.querySelector(".quadro-07");
+quadro07.addEventListener("click", function(){
+    quadro07.classList.toggle("verde")
+})
+
+const quadro08 = document.querySelector(".quadro-08");
+quadro08.addEventListener("click", function(){
+    quadro08.classList.toggle("verde")
+})
+
+const quadro09 = document.querySelector(".quadro-09");
+quadro09.addEventListener("click", function(){
+    quadro09.classList.toggle("verde")
+})
